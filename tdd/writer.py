cef7b9338f2743e470c9a83b937f2a369ce920c6
pocin <nemeth.robin@gmail.com>
2018-04-11 16:30:39 +0200

    WIP - workflow for creating campaigns and adgroups
    untested and I still need to figure out out to map campaign to adgroups in input
    tables (maybe we will have to load everything into memory

--- tdd/writer.py
+++ tdd/writer.py
@@ -1,4 +1,78 @@
+"""
+KBC Related stuff
+
+"""
 import logging
+import sys
+from pathlib import Path
+from tdd.client import KBCTDDClient
+import tdd.models
+from keboola.docker import Config
 
 def main():
     logging.info("Hello, world!")
+
+
+
+def _main(datadir):
+    cfg = Config(datadir)
+    params = cfg.get_parameters()
+    _datadir = Path(datadir)
+    intables = _datadir / 'in/tables'
+    if params.get('debug'):
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    else:
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    path_csv_log = _datadir / 'out/tables/tdd_writer_log.csv'
+
+    writer = TDDWriter(login=params['login'],
+                       password=params['#password'],
+                       path_csv_log=path_csv_log)
+
+    path_campaigns = intables / 'create_campaigns.csv'
+    logging.info("Validating campaign input @ %s", path_campaigns)
+    # first pass is to check it's ok
+    # TODO: maybe we actually need to load it into memory
+    # to match campaigns and adgroups based on common ID (hash)?
+    for _ in writer.validate_campaign_input(path_campaigns):
+        pass
+
+    logging.info("Campaign input seems to be OK")
+
+    path_adgroup = intables / 'crate_adgroup.csv'
+    # at this point the adgroup code doesn't contain the campaign id
+    # (which is unknown, yet)
+    logging.info("Validating AdGroup input @ %s", path_adgroup)
+    for _ in writer.validate_adgroup_input(path_adgroup):
+        pass
+    logging.info("AdGroup input seems to be OK")
+
+    # TODO: how to actually link campaign to adgroup[s] (is this 1:N?)
+    # in original tables?
+
+class TDDWriter(KBCTDDClient):
+    """
+    Implements
+    - config file processing logic
+    - partial input validation
+    """
+
+    def validate_campaign_input(self, path_csv):
+        yield from tdd.models.validate_input_csv(
+            path_csv,
+            schema=tdd.models.CreateCampaignSchema)
+
+
+    def validate_adgroup_input(self, path_csv):
+        yield from tdd.models.validate_input_csv(
+            path_csv,
+            schema=tdd.models.CreateAdGroupSchema)
+
+    def create_campaign_and_adgroup(self, campaign_payload, adgroup_payload):
+        """
+        This expects that adgroup_payload is actually valid
+
+        """
+        new_campaign = self.create_campaign(campaign_payload)
+        adgroup_payload['CampaignID'] = new_campaign['CampaignID']
+        self.create_adgroup(adgroup_payload)
---
+++ tests/test_writer_validation.py
@@ -0,0 +1,10 @@
+"""
+Partial validations related to cerating workflow
+
+"""
+import pytest
+
+from tdd.writer import TDDWriter
+
+def test_validating_creating_campaign():
+    pass
